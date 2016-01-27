f27d05d5242dd75a28843fb3799050b0de6a2532
bescione <claudioalberti@outlook.com>
2016-01-27 13:34:44 +0100

    Create login.php

---
+++ 27-gennaio/login.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<form method="GET" action="elaboralogin.php">
+			<label for="username">Nome utente</label>
+			<input type="text" name="username" id="username">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password">
+			<input type="submit" value="Invia">
+		</form>
+	</body>
+</html>
