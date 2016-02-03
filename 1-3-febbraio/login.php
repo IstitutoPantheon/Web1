921f8c486c00b7d2f25ac626e08f8aee936dfb1b
bescione <claudioalberti@outlook.com>
2016-02-03 12:54:30 +0100

    Create login.php

---
+++ 1-3-febbraio/login.php
@@ -0,0 +1,7 @@
+<?php
+include'inc/header.php';
+?>
+<form method="post" action="assets/gestiscilogin.php">
+	<input type="text" name="nomeUtente">
+	<input type="password" name="passwordUtente">
+</form>
